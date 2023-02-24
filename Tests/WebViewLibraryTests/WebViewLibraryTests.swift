e0342dfa9d8613fa846ce297b6816f2e3ad73943
tesddev <tesleem.amuda@gmail.com>
2023-02-24 12:29:49 +0100

    add test logic

--- Tests/WebViewLibraryTests/WebViewLibraryTests.swift
+++ Tests/WebViewLibraryTests/WebViewLibraryTests.swift
@@ -6,6 +6,7 @@ final class WebViewLibraryTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(WebViewLibrary().text, "Hello, World!")
+        let mock = WebViewLibrary(link: "fb.com")
+        XCTAssertNotNil(mock.link, "link not provided")
     }
 }
